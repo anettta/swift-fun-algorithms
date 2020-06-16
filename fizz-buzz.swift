239eda0be6a302b14367e15e9c85a7db71b075bf
Anya <annagapyuk@gmail.com>
2020-06-16 15:15:56 -0400

    created oneThousandNumbers array

--- fizz-buzz.swift
+++ fizz-buzz.swift
@@ -2,15 +2,23 @@ import UIKit
 
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-for num in numbers {
-    if num % 3 == 0 {
+var oneThousandNumbers = [Int]()
+for i in 1...1000 {
+    oneThousandNumbers.append(1)
+}
+
+
+for num in oneThousandNumbers {
+    if num % 3 == 0 && num % 5 == 0 {
+     print("\(num) fizzbuzz")
+    } else if num % 3 == 0 {
         print("\(num) fizz")
     } else if num % 5 == 0 {
         print("\(num) buzz")
-    } else {
-     print(num)
+    }  else {
+        print(num)
     }
-    
+
    // 15 => fizzbuzz
     
 }
